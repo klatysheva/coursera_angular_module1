bb1c444acf03c56683e5e37bb9e7fc051ce1eea7
Kseniia Latysheva <kseniia.latysheva@t-systems.com>
2022-01-02 23:51:54 +0300

    Add assignment 1 solution

---
+++ app.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController)
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.food = "";
+        $scope.foodCount = "0";
+        $scope.msg = "";
+
+        $scope.checkIfTooMuch = function () {
+            $scope.foodCount = $scope.countFood();
+            if ($scope.foodCount  > 3) {
+                $scope.msg = "Too much!"
+            }
+            else {
+                $scope.msg = "Enjoy!"
+            }
+        };
+
+        $scope.countFood = function () {
+            const foodArr = $scope.food.split(',');
+            return foodArr.length;
+        }
+
+
+        //
+        // $scope.feedYaakov = function () {
+        //     $scope.stateOfBeing = "fed";
+        // };
+    }
+})();
